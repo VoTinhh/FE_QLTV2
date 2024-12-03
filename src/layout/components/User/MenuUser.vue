ac9afbed2362375d0b64df8074cf39cf47428c56
Hiếu Huỳnh <hieu47471029@gmail.com>
2024-12-03 15:49:02 +0700

    Hieu2

--- src/layout/components/User/MenuUser.vue
+++ src/layout/components/User/MenuUser.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="nav-container primary-menu">
+  <div class="nav-container primary-menu áđâs" :style="{ position: 'absolute' }">
         <div class="mobile-topbar-header">
             <div>
                 <img src="../../assets/images/anhNen.png" class="logo-icon" alt="logo icon">
@@ -11,26 +11,6 @@
         </div>
         <nav class="navbar navbar-expand-xl w-100">
             <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
-                <!-- Thêm mới menu -->
-                <li class="nav-item dropdown">
-                    <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
-                        data-bs-toggle="dropdown">
-                        <div class="parent-icon"><i class='bx bx-home-circle'></i></div>
-                        <div class="menu-title">Thêm mới</div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <router-link class="dropdown-item" to="/admin/ban-bida">
-                                <i class="bx bx-right-arrow-alt"></i> Bàn
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link class="dropdown-item" to="/admin/phong-bida">
-                                <i class="bx bx-right-arrow-alt"></i> Phòng
-                            </router-link>
-                        </li>
-                    </ul>
-                </li>
                 <!-- Các mục khác -->
                 <li class="nav-item">
                     <router-link class="nav-link" to="/user/lich-su-muon">
@@ -76,40 +56,6 @@
             </ul>
         </nav>
     </div>
-  <div class="nav-container primary-menu áđâs" :style="{ position: 'absolute' }">
-    <div class="mobile-topbar-header">
-      <div>
-        <img src="../../assets/images/anhNen.png" class="logo-icon" alt="logo icon" />
-      </div>
-      <div>
-        <h4 class="logo-text">Rukada</h4>
-      </div>
-      <div class="toggle-icon ms-auto">
-        <i class="bx bx-arrow-to-left"></i>
-      </div>
-    </div>
-    <nav class="navbar navbar-expand-xl w-100">
-      <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
-        <!-- Các mục khác -->
-        <li class="nav-item">
-          <router-link class="nav-link" to="/user/lich-su-muon">
-            <div class="parent-icon">
-              <i class="fa-solid fa-table"></i>
-            </div>
-            <div class="menu-title">Lịch sử mượn</div>
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/user/thanh-toan">
-            <div class="parent-icon">
-              <i class="fa-solid fa-table"></i>
-            </div>
-            <div class="menu-title">Thanh toán</div>
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-  </div>
 </template>
 
 <script>
